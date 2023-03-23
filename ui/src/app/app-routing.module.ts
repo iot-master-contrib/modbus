f3ea6a9fdbb4a6a032dcda6d9415a8dd3b81c8c5
EnTrouble <1498829223@qq.com>
2023-03-23 11:05:57 +0800

    增加产品和设置模块

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -6,30 +6,37 @@ import { SerialComponent } from './admin/serial/serial.component';
 import { ServerComponent } from './admin/server/server.component';
 import { ClientComponent } from './admin/client/client.component';
 import { MapperComponent } from './admin/mapper/mapper.component';
-import { PollerComponent } from './admin/poller/poller.component'; 
+import { PollerComponent } from './admin/poller/poller.component';
+import { ProductComponent } from './admin/product/product.component';
+const pages: Routes = [
+  {
+    path: 'setting',
+    loadChildren: () => import('./admin/setting/setting.module').then(m => m.SettingModule)
+  }
+]
 const routes: Routes = [
   {
     path: 'admin',
-    component: AdminComponent,children:[
+    component: AdminComponent,
+    children: [
       { path: 'server', component: ServerComponent },
       { path: 'serial', component: SerialComponent },
       { path: 'client', component: ClientComponent },
       { path: 'mapper', component: MapperComponent },
       { path: 'poller', component: PollerComponent },
-      { path: 'link', component: LinkComponent }, 
-      { path: '**', redirectTo:'server' },
+      { path: 'link', component: LinkComponent },
+      { path: 'product', component: ProductComponent },
+      ...pages,
+      { path: '**', redirectTo: 'server' },
     ]
   },
   {
-    path: '**',redirectTo:'admin'
-   
+    path: '**', redirectTo: 'admin'
   },
-  
-  
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { zh_CN } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AdminComponent } from './admin/admin.component';
@@ -16,15 +16,16 @@ import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzModalModule } from 'ng-zorro-antd/modal';
 import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzDropDownModule } from 'ng-zorro-antd/dropdown'; 
-import { NzFormModule } from 'ng-zorro-antd/form';  
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzFormModule } from 'ng-zorro-antd/form';
 import { SerialComponent } from './admin/serial/serial.component';
 import { ServerComponent } from './admin/server/server.component';
 import { ClientComponent } from './admin/client/client.component';
 import { MapperComponent } from './admin/mapper/mapper.component';
 import { PollerComponent } from './admin/poller/poller.component';
+import { ProductComponent } from './admin/product/product.component';
 import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
-import { NzSelectModule } from 'ng-zorro-antd/select'; 
+import { NzSelectModule } from 'ng-zorro-antd/select';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzTreeModule } from 'ng-zorro-antd/tree';
@@ -34,22 +35,30 @@ import { MapperFmComponent } from './admin/form/mapper-fm/mapper-fm.component';
 import { PollerFmComponent } from './admin/form/poller-fm/poller-fm.component';
 import { SerialFmComponent } from './admin/form/serial-fm/serial-fm.component';
 import { ServerFmComponent } from './admin/form/server-fm/server-fm.component';
-import { NzMessageModule } from 'ng-zorro-antd/message'; 
+import { NzMessageModule } from 'ng-zorro-antd/message';
 import { NzNotificationModule } from 'ng-zorro-antd/notification';
 import { NzPopconfirmModule } from 'ng-zorro-antd/popconfirm';
-import { LinkFmComponent } from './admin/form/link-fm/link-fm.component'; 
+import { LinkFmComponent } from './admin/form/link-fm/link-fm.component';
 import { NzSpaceModule } from 'ng-zorro-antd/space';
 registerLocaleData(zh);
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent, 
+    AdminComponent,
     SerialComponent,
     ServerComponent,
     ClientComponent,
-    MapperComponent, 
-    PollerComponent, LinkComponent, ClientFmComponent, MapperFmComponent, PollerFmComponent, SerialFmComponent, ServerFmComponent, LinkFmComponent,  
+    MapperComponent,
+    PollerComponent,
+    LinkComponent,
+    ClientFmComponent,
+    MapperFmComponent,
+    PollerFmComponent,
+    SerialFmComponent,
+    ServerFmComponent,
+    LinkFmComponent,
+    ProductComponent,
   ],
   imports: [
     BrowserModule,
@@ -59,24 +68,24 @@ registerLocaleData(zh);
     BrowserAnimationsModule,
     NzLayoutModule,
     NzMenuModule,
-    NzIconModule ,
+    NzIconModule,
     NzTableModule,
     NzTreeModule,
     NzSpaceModule,
     NzMessageModule,
-    NzNotificationModule , 
+    NzNotificationModule,
     NzModalModule,
     NzDividerModule,
     NzButtonModule,
     NzDropDownModule,
-    NzPopconfirmModule ,
+    NzPopconfirmModule,
     NzFormModule,
     ReactiveFormsModule,
     NzPageHeaderModule,
-    NzSelectModule 
+    NzSelectModule
   ],
   providers: [
-    { provide: NZ_I18N, useValue: zh_CN } 
+    { provide: NZ_I18N, useValue: zh_CN }
   ],
   bootstrap: [AppComponent]
 })
