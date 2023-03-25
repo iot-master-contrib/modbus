a2ec376369869bcef9448eaecc73624daac2c22a
jason <jason@zgwit.com>
2023-03-25 16:45:23 +0800

    添加协议文件，支持RTU TCP和并行TCP

---
+++ internal/messager.go
@@ -0,0 +1,33 @@
+package internal
+
+import (
+	"io"
+	"sync"
+)
+
+type Messenger struct {
+	mu   sync.Mutex
+	Conn io.ReadWriter
+}
+
+func (m *Messenger) Ask(request []byte, response []byte) (int, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	_, err := m.Conn.Write(request)
+	if err != nil {
+		return 0, err
+	}
+	return m.Conn.Read(response)
+}
+
+func (m *Messenger) AskAtLeast(request []byte, response []byte, min int) (int, error) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	_, err := m.Conn.Write(request)
+	if err != nil {
+		return 0, err
+	}
+	return io.ReadAtLeast(m.Conn, response, min)
+}
