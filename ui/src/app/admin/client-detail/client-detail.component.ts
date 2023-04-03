fcd79ece827c31924225eeac3e68ece5b54fa106
jason <jason@zgwit.com>
2023-04-03 20:38:29 +0800

    客户端

--- ui/src/app/admin/client-detail/client-detail.component.ts
+++ ui/src/app/admin/client-detail/client-detail.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, RouterState} from "@angular/router";
+import {RequestService} from "../../request.service";
 
 @Component({
   selector: 'app-client-detail',
   templateUrl: './client-detail.component.html',
   styleUrls: ['./client-detail.component.scss']
 })
-export class ClientDetailComponent {
+export class ClientDetailComponent implements OnInit {
+  id: string = ""
+  data: any = {}
+
+  constructor(private route: ActivatedRoute, private rs: RequestService) {
+  }
+
+  ngOnInit(): void {
+    // @ts-ignore
+    this.id = this.route.snapshot.paramMap.get("id")
+
+    this.load()
+  }
+
+  load() {
+    this.rs.get(`client/${this.id}`).subscribe(res => {
+      this.data = res.data;
+    })
+  }
 
 }
