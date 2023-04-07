6f611d6d0735a551404bcabed5f4577977c31b5f
Jason <jason@zgwit.com>
2023-04-07 18:00:15 +0800

    缺少文件

---
+++ ui/src/app/admin/tunnel-edit-retry/tunnel-edit-retry.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tunnel-edit-retry',
+  templateUrl: './tunnel-edit-retry.component.html',
+  styleUrls: ['./tunnel-edit-retry.component.scss']
+})
+export class TunnelEditRetryComponent {
+
+}
