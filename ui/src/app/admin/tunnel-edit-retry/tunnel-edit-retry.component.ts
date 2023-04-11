3a4e6ed43f087d63a4b5768833962dc81438fd53
T-r-ee <1311337083@qq.com>
2023-04-11 17:25:01 +0800

    重试编辑

--- ui/src/app/admin/tunnel-edit-retry/tunnel-edit-retry.component.ts
+++ ui/src/app/admin/tunnel-edit-retry/tunnel-edit-retry.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { RequestService } from 'src/app/request.service';
 
 @Component({
   selector: 'app-tunnel-edit-retry',
   templateUrl: './tunnel-edit-retry.component.html',
-  styleUrls: ['./tunnel-edit-retry.component.scss']
+  styleUrls: ['./tunnel-edit-retry.component.scss'],
 })
 export class TunnelEditRetryComponent {
-
+  constructor(private fb: FormBuilder, private msg: NzMessageService,private rs: RequestService,) {}
+  @Input()url!:string
+  validateForm!: FormGroup;
+  setting() {
+    // this.rs.post(this.url, this.validateForm.value).subscribe((res) => {
+    //   this.msg.success('设置成功'); 
+    // }); 
+  }
+  patchValue(mess?: any) {
+    mess = mess || {};
+    this.validateForm = this.fb.group({
+      timeout: [mess.timeout || 0],
+      enable: [mess.enable || true],
+    });
+  }
 }
