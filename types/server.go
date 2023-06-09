76262badc17c8d314ecaece7e059635aa343c468
Jason <jason@zgwit.com>
2023-06-09 11:01:32 +0800

    在

--- types/server.go
+++ types/server.go
@@ -4,7 +4,7 @@ type Server struct {
 	Tunnel     `xorm:"extends"`
 	Port       uint16          `json:"port,omitempty"`       //监听端口
 	Standalone bool            `json:"standalone,omitempty"` //单例模式（不支持注册）
-	Devices    []DefaultDevice `json:"devices,omitempty"`
+	Devices    []DefaultDevice `json:"devices,omitempty" xorm:"json"`
 }
 
 type DefaultDevice struct {
